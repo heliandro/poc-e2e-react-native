4ba0953702a0e43a9c3ee09340e02865b211ba94
Heliandro Lavôr <heliandro.l@gmail.com>
2022-09-08 23:53:03 -0300

    feat: add react navigation, cofiguracao e uso no app component (#2)

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './src/telas/Home';
 import MockTextoService from './src/servicos/MockTextoService';
@@ -6,18 +8,23 @@ import MockUrlService from './src/servicos/MockUrlService';
 import useTextos from './src/hooks/useTextos';
 import useUrls from './src/hooks/useUrls';
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
 
   const mockTextoService = new MockTextoService();
   const mockUrlService = new MockUrlService();
  
   return (
-    <>
-      <Home 
-        mockTextoService={mockTextoService} mockUrlService={mockUrlService}
-        useTextos={useTextos} useUrls={useUrls}
-      />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home">
+          {() => <Home mockTextoService={mockTextoService} mockUrlService={mockUrlService}
+          useTextos={useTextos} useUrls={useUrls}/>}
+        </Stack.Screen>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- android/app/src/main/java/com/poce2ereactnative/MainActivity.java
+++ android/app/src/main/java/com/poce2ereactnative/MainActivity.java
@@ -1,10 +1,18 @@
 package com.poce2ereactnative;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+  // Necessario para que o React Navigation funcione corretamente.
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
